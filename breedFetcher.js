586352130f8133622e972012468dd77dd97fa75d
Amoazzami <moazzami.azadeh@gmail.com>
2020-09-24 21:07:40 +0000

    test added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,25 @@
 const request = require('request');
 
-// implementing in Node with GET 
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName , function(error, response, body) {
+//     if (error) {
+//       return "there is an error";
+//     }
+//     //console.log(body);
+       const data = JSON.parse(body);
+       callback(error, data[0].description)
 
-let breeds = process.argv.splice(2);
-request("https://api.thecatapi.com/v1/breeds/search?q=" + breeds , function(error, response, body){
-  if (error) {
-  return console.log("there is an error");
-  }
-  console.log(body);
-  const data = JSON.parse(body);
-  console.log(data, "converted string");
-  console.log(typeof data, "displays the type of data");
-  //console.log(data[0].description, "accesses the first element of an array and returns description");
-  if (data.length === 0 ) {
-    console.log("not a breed");
-  }
-});
+//     // console.log(data, "converted string");
+//     // console.log(typeof data, "displays the type of data");
+//     // console.log(data[0].description, "accesses the first element of an array and returns description");
+//     if (data.length === 0) {
+//       return "not a breed"; 
+//     } else {
+//       return data[0].description;
+
+//     }
+
+ });
+}
+
+module.exports = { fetchBreedDescription };
